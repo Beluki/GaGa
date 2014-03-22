f942c95b347caea218079cc991ad6bb774c284a5
Beluki <beluki@gmx.com>
2014-03-22 19:47:06 +0100

    Added LineReader and a default streams.ini.

---
+++ Source/GaGa/LineReader.cs
@@ -0,0 +1,45 @@
+﻿
+// GaGa.
+
+using System;
+using System.IO;
+
+
+namespace GaGa
+{
+    /// <summary>
+    /// A simple stream reader that keeps track of the current line number.
+    /// </summary>
+    internal class LineReader
+    {
+        private StreamReader reader;
+        private int line;
+
+        public LineReader(Stream stream)
+        {
+            reader = new StreamReader(stream);
+            line = 0;
+        }
+
+        /// <summary>
+        /// Current line number.
+        /// </summary>
+        public int LineNumber()
+        {
+            return line;
+        }
+
+        /// <summary>
+        /// Read a line from the stream.
+        /// </summary>
+        /// <returns>
+        /// Line text as a string.
+        /// </returns>
+        public String ReadLine()
+        {
+            line++;
+            return reader.ReadLine();
+        }
+    }
+}
+
