3b43c21d37e3b8651c247c29a91af828e0d8a528
Beluki <beluki@gmx.com>
2015-01-30 14:23:00 +0100

    Fixed incorrect year in assembly file version.

--- Source/GaGa/Assembly/AssemblyInfo.cs
+++ Source/GaGa/Assembly/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("GaGa Radio Player")]
 [assembly: AssemblyProduct("GaGa")]
 [assembly: AssemblyVersion("2015.01.30")]
-[assembly: AssemblyFileVersion("2014.01.30")]
+[assembly: AssemblyFileVersion("2015.01.30")]
 
